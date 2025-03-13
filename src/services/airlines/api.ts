e2ec5bc8f503cf8323054a1c905c81e6e20035f2
mmamrutlt <mauricio.mamrut@lightit.io>
2025-03-13 09:27:55 -0300

    fix: types fixes

--- src/services/airlines/api.ts
+++ src/services/airlines/api.ts
@@ -1,30 +1,14 @@
 import { privateApi } from '@/config/api';
-import type { BackendFlight } from '../flights/types';
 import type { RequestParams, ServiceResponse } from '../types';
+import { type AirlineResponse, transformAirlineData } from './transformAirlineData';
 import type { Airline } from './types';
-
-interface BackendAirline {
-  id: number;
-  name: string;
-  description: string;
-  flights: BackendFlight[];
-}
-
-const transformAirlineData = (airline: BackendAirline): Airline => {
-  return {
-    id: String(airline.id),
-    name: airline.name,
-    description: airline.description,
-    flights_count: airline.flights.length,
-  };
-};
 
 export const getAirlinesList = async ({
   page,
   pageSize,
   searchText,
 }: RequestParams): Promise<ServiceResponse<Airline[]>> => {
-  const response = await privateApi.get<ServiceResponse<BackendAirline[]>>('airlines', {
+  const response = await privateApi.get<ServiceResponse<AirlineResponse[]>>('airlines', {
     params: { page, pageSize, searchText },
   });
 
